f2bfaa4aeabfc3b1ef2c5603fc74279bef3f0ecf
User 1 <syedayanali28@gmail.com>
2023-01-11 11:45:01 +0800

    Added a Animal superclass which will be inherited by other classes later

---
+++ Animal.py
@@ -0,0 +1,17 @@
+class Animal:
+    def __init__(self):
+        self.has_legs = False
+        self.num_eyes = 0
+
+    def breath():
+        print("Breathing")
+
+    def walk():
+        print("Walking")
+
+    def drink():
+        print("Drinking")
+
+    def sleep():
+        print("Sleeping")
+    
